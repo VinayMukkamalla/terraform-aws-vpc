c46a26edf822a2840b3178962017546b869b4755
VinayMukkamalla <vinay.mukkamalla@gmail.com>
2025-11-09 14:54:59 +0530

    terraform module for vpc peering

--- outputs.tf
+++ outputs.tf
@@ -9,7 +9,7 @@ output "public_subet_ids" {
 
 
 #providing private subnet ids as output to display on console
-output "public_subet_ids" {
+output "private_subet_ids" {
   value = module.aws_subnet.private[*].id
 }
 
