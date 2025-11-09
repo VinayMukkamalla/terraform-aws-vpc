7ce9266c1befe8f86a9cac34a29f53f2c2e394b2
VinayMukkamalla <vinay.mukkamalla@gmail.com>
2025-11-09 13:18:17 +0530

    using terraform module for vpc peering

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,20 @@
 output "vpc_id" {
   value = aws_vpc.main.id  # here main or this is modeule name of VPC
 }
+
+#providing public subnet ids as output to display on console
+output "public_subet_ids" {
+  value = module.aws_subnet.public[*].id
+}
+
+
+#providing private subnet ids as output to display on console
+output "public_subet_ids" {
+  value = module.aws_subnet.private[*].id
+}
+
+
+#providing database subnet ids as output to display on console
+output "database_subet_ids" {
+  value = module.aws_subnet.database[*].id
+}
